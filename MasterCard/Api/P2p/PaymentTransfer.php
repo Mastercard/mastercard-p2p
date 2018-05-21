da0f430ad255872079b5a875578e51447d98d732
ade90036 <andrea.rizzini@mastercard.com>
2018-05-21 06:46:56 -0500

    incremental commit

---
+++ MasterCard/Api/P2p/PaymentTransfer.php
@@ -0,0 +1,125 @@
+<?php
+/*
+ * Copyright 2016 MasterCard International.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ * Neither the name of the MasterCard International Incorporated nor the names of its
+ * contributors may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+ namespace MasterCard\Api\P2p;
+
+ use MasterCard\Core\Model\BaseObject;
+ use MasterCard\Core\Model\RequestMap;
+ use MasterCard\Core\Model\OperationMetadata;
+ use MasterCard\Core\Model\OperationConfig;
+
+
+/**
+ * 
+ */
+class PaymentTransfer extends BaseObject {
+
+
+
+	protected static function getOperationConfig($operationUUID) {
+		switch ($operationUUID) {
+			case "40befaab-77f9-4ef9-ac92-b73a5812fe82":
+				return new OperationConfig("/send/v1/partners/{partnerId}/transfers/payment", "create", array(), array());
+			case "a41d8939-3f52-47c2-8371-e3a31cd8b3c6":
+				return new OperationConfig("/send/v1/partners/{partnerId}/transfers/{transferId}", "read", array(), array());
+			case "7116b2ec-311b-4035-a43f-1070bfbf34e0":
+				return new OperationConfig("/send/v1/partners/{partnerId}/transfers", "query", array("ref"), array());
+			
+			default:
+				throw new \Exception("Invalid operationUUID supplied: $operationUUID");
+		}
+	}
+
+	protected static function getOperationMetadata() {
+		$config = ResourceConfig::getInstance();
+		return new OperationMetadata($config->getVersion(), $config->getHost(), $config->getContext(), $config->getJsonNative(), $config->getContentTypeOverride());
+	}
+
+
+   /**
+	* Creates object of type PaymentTransfer
+	*
+	* @param Map map, containing the required parameters to create a new object
+	* @return PaymentTransfer of the response of created instance.
+	*/
+	public static function create($map)
+	{
+		return self::execute("40befaab-77f9-4ef9-ac92-b73a5812fe82", new PaymentTransfer($map));
+	}
+
+
+
+
+
+
+
+
+
+	/**
+	 * Returns objects of type PaymentTransfer by id and optional criteria
+	 * @param type $id
+	 * @param type $criteria
+	 *
+	 * @throws ApiException - which encapsulates the http status code and the error return by the server
+	 *
+	 * @return PaymentTransfer of the response
+	 */
+	public static function readByID($id, $criteria = null)
+	{
+		$map = new RequestMap();
+		if (!empty($id)) {
+			$map->set("id", $id);
+		}
+		if ($criteria != null) {
+			$map->setAll($criteria);
+		}
+		return self::execute("a41d8939-3f52-47c2-8371-e3a31cd8b3c6",new PaymentTransfer($map));
+	}
+
+
+
+
+
+
+	/**
+	 * Query objects of type PaymentTransfer by id and optional criteria
+	 *
+	 * @param type $criteria
+	 *
+	 * @throws ApiException - which encapsulates the http status code and the error return by the server
+	 *
+	 * @return PaymentTransfer of the response
+	 */
+	public static function readByReference($criteria)
+	{
+		return self::execute("7116b2ec-311b-4035-a43f-1070bfbf34e0",new PaymentTransfer($criteria));
+	}
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# p2p, latest release version:1.0.3
